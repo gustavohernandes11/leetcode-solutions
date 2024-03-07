9a53abefd9ee79ace1871bcafd44436c1780a564
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2024-03-07 15:41:48 -0300

    feat: add FindTheIndexOfTheFirstOccurrenceInAString another solution

---
+++ FindTheIndexOfTheFirstOccurrenceInAString/buildInMethod.ts
@@ -0,0 +1,2 @@
+const strStr = (haystack: string, needle: string): number =>
+	haystack.indexOf(needle);
--- FindTheIndexOfTheFirstOccurrenceInAString/index.ts
+++ FindTheIndexOfTheFirstOccurrenceInAString/index.ts
@@ -1,2 +1,14 @@
-const strStr = (haystack: string, needle: string): number =>
-	haystack.indexOf(needle);
+function strStr(haystack: string, needle: string): number {
+	const haystackLength = haystack.length;
+	const needleLength = needle.length;
+
+	if (haystackLength < needleLength) return -1;
+	for (let i = 0; i <= haystackLength - needleLength; i++) {
+		let j = 0;
+		while (j < needleLength && haystack[i + j] === needle[j]) {
+			j++;
+		}
+		if (j === needle.length) return i;
+	}
+	return -1;
+}
